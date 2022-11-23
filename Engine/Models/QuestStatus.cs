610b75c112438367b9468cf4d134521b393ea2fe
oleh.strokan <oleh.strokan@accenture.com>
2022-11-23 17:08:49 +0100

    Add quest, quest status, quest factory, item quantity classes

---
+++ Engine/Models/QuestStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace Engine.Models;
+
+public class QuestStatus
+{
+    public Quest PlayerQuest { get; set; }
+    public bool isComplete { get; set; }
+
+    public QuestStatus(Quest quest)
+    {
+        PlayerQuest = quest;
+        isComplete = false;
+    }
+}
