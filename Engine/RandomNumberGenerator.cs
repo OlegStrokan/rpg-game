9b165d10d1d6d3e95b323859a92d94cd1c7886d4
oleh.strokan <oleh.strokan@accenture.com>
2022-11-23 17:38:16 +0100

    add random number generator

---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Engine;
+
+public class RandomNumberGenerator
+{
+    private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+
+    public static int NumberBetween(int minimumValue, int maximumValue)
+    {
+        byte[] randonNumber = new byte[1];
+        
+        _generator.GetBytes(randonNumber);
+
+        double asciiValueOfRandomCharacter = Convert.ToDouble(randonNumber[0]);
+
+        double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+
+        int range = maximumValue - minimumValue + 1;
+
+        double randomValueInRange = Math.Floor(multiplier * range);
+
+        return (int)(minimumValue + randomValueInRange);
+    }
+}
