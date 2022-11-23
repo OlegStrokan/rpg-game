3d3f3d9a6edf5950c8df74c7b5e15ad4634d43ab
oleh.strokan <oleh.strokan@accenture.com>
2022-11-23 17:37:48 +0100

    - add monster and moster factory classes - update item factory

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -13,8 +13,12 @@ public static class ItemFactory
          _standartGameItems = new List<GameItem>();
          _standartGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
          _standartGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
-         _standartGameItems.Add(new GameItem(9001, "Snake fang", 5 ));
-         _standartGameItems.Add(new GameItem(9002, "Snakeskin", 5 ));
+         _standartGameItems.Add(new GameItem(9001, "Snake fang", 1 ));
+         _standartGameItems.Add(new GameItem(9002, "Snakeskin", 2 ));
+         _standartGameItems.Add(new GameItem(9003, "Rat tail", 1 ));
+         _standartGameItems.Add(new GameItem(9004, "Rat fur", 2 ));
+         _standartGameItems.Add(new GameItem(9005, "Spider fang", 1 ));
+         _standartGameItems.Add(new GameItem(9006, "Spider silk", 2 ));
      }
 
      public static GameItem CreateGameItem(int itemTypeID)
---
+++ Engine/Models/Monster.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Engine.Models;
+
+public class Monster : BaseNotification
+{
+    private int _hitPoints;
+    
+    public string Name { get; set; }
+    public string ImageName { get; set; }
+    public int MaximumHitPoints { get; set; }
+
+    public int HitPoints
+    {
+        get { return _hitPoints; }
+        private set
+        {
+            _hitPoints = value;
+            OnPropertyChanged(nameof(HitPoints));
+        }
+    }
+    
+    public int RewardExperiencePoints { get; private set; }
+    public int RewardGold { get; private set; }
+    
+    public ObservableCollection<ItemQuantity> Inventory { get; set; }
+
+    public Monster(
+        string name,
+        string imageName, 
+        int maximumHitPoints,
+        int hitPoints, 
+        int rewardExperiencePoints,
+        int rewardGold
+        )
+    {
+        Name = name;
+        ImageName = string.Format("/Engine;component/Images/Monsters/{0}", imageName);
+        MaximumHitPoints = maximumHitPoints;
+        HitPoints = hitPoints;
+        RewardExperiencePoints = rewardExperiencePoints;
+        RewardGold = rewardGold;
+
+        Inventory = new ObservableCollection<ItemQuantity>();
+    }
+}
