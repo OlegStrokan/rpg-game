83bac114376ccc90921e2632073b18b8d2a035c3
oleh.strokan <oleg14ua71@gmail.com>
2022-12-20 21:17:51 +0100

    Add ConvertData class

---
+++ Engine/Utils/ConvertData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Engine.Models;
+
+namespace Engine.Utils;
+
+public static class ConvertData
+{
+    public static string[] Items;
+
+    public static GameItem ConverToObject(string name)
+    {
+        Items = name.Split(" ");
+        GameItem item = new GameItem(Int32.Parse(Items[0]), Items[1] , Int32.Parse(Items[2]));
+        return item;
+    }
+}
