50aa3b289bd41c522df84ecf78551251210d6b86
oleh.strokan <oleg14ua71@gmail.com>
2022-12-22 12:45:53 +0100

    Add GroupedInventoryItem

--- Engine/Models/LivingEntity.cs
+++ Engine/Models/LivingEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 
 namespace Engine.Models;
@@ -53,24 +54,51 @@ public abstract class LivingEntity : BaseNotification
     }
     
     public ObservableCollection<GameItem> Inventory { get; set; }
+    public ObservableCollection<GroupedInventoryItem> GroupedInventory { get; set; }
 
     public List<GameItem> Weapons => Inventory.Where(i => i is Weapon).ToList();
 
     protected LivingEntity()
     {
         Inventory = new ObservableCollection<GameItem>();
+        GroupedInventory = new ObservableCollection<GroupedInventoryItem>();
     }
 
     public void AddItemToInventory(GameItem item)
     {
         Inventory.Add(item);
-        
+
+        if (item.IsUnique)
+        {
+            GroupedInventory.Add(new GroupedInventoryItem(item, 1));
+        }
+        else
+        {
+            if (!GroupedInventory.Any(gi => gi.Item.ItemTypeID == item.ItemTypeID))
+            {
+                GroupedInventory.Add(new GroupedInventoryItem(item, 0));
+            }
+
+            GroupedInventory.First(gi => gi.Item.ItemTypeID == item.ItemTypeID).Quantity++;
+        }
+
         OnPropertyChanged(nameof(Weapons));
     }
 
     public void RemoveItemFromInventory(GameItem item)
     {
         Inventory.Remove(item);
+
+        GroupedInventoryItem groupedInventoryItemToRemove = GroupedInventory.FirstOrDefault(gi => gi.Item == item);
+
+        if (groupedInventoryItemToRemove.Quantity == 1)
+        {
+            GroupedInventory.Remove(groupedInventoryItemToRemove);
+        }
+        else
+        {
+            groupedInventoryItemToRemove.Quantity--;
+        }
         
         OnPropertyChanged(nameof(Weapons));
     }
--- Engine/Models/Weapon.cs
+++ Engine/Models/Weapon.cs
@@ -6,7 +6,7 @@ public class Weapon : GameItem
     public int MinimumDamage { get; set; }
     public int MaximumDamage { get; set; }
     
-    public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage) : base(itemTypeID, name, price)
+    public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage) : base(itemTypeID, name, price, true)
     {
         MinimumDamage = minDamage;
         MaximumDamage = maxDamage;
