4e8ca34d695f98388137a4bda048ad9acf7ca1a1
oleh.strokan <oleg14ua71@gmail.com>
2022-12-23 20:05:55 +0100

    Fix convertData function (add weapon support)

--- Engine/Utils/ConvertData.cs
+++ Engine/Utils/ConvertData.cs
@@ -12,24 +12,24 @@ public static class ConvertData
     public static List<string> Line = new List<string>();
     public static GameItem Item;
 
-    public static List<GameItem> ConverToGameItem(List<string> items)
+    public static List<GameItem> ConverToObject(List<string> items)
     {
         foreach (string item in items)
         {
             Line = item.Split(",").ToList();
-            Item = new GameItem(Int32.Parse(Line[0]), Line[1] , Int32.Parse(Line[2]));
+            if (Line.Count == 3)
+            {
+                Item = new GameItem(Int32.Parse(Line[0]), Line[1], Int32.Parse(Line[2]));
+            }
+            else
+            {
+                Item = new Weapon(Int32.Parse(Line[0]), Line[1], Int32.Parse(Line[2]), Int32.Parse(Line[3]), Int32.Parse(Line[4]));
+            }
+
             Items.Add(Item);
-            
+
         }
+
         return Items;
-    }  
-    // public static List<GameItem> ConverToWeapon(List<string> name)
-    // {
-    //     while (name != null)
-    //     {
-    //         
-    //     }
-    //     GameItem item = new GameItem(Int32.Parse(Items[0]), Items[1] , Int32.Parse(Items[2]));
-    //     return item;
-    // }
+    }
 }
