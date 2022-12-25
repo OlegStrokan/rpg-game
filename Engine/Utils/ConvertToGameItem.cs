5f2be72a203379b8137c513d799cdc2879009e8a
oleh.strokan <oleg14ua71@gmail.com>
2022-12-25 14:21:44 +0100

    Add Convert to Game item class

---
+++ Engine/Utils/ConvertToGameItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Engine.Models;
+using Newtonsoft.Json;
+
+namespace Engine.Utils;
+
+public static class ConvertToGameItem
+{
+    public static List<GameItem> GameItems = new List<GameItem>();
+    public static string basePath = @"C:\Users\oleh.strokan\RiderProjects\rpg-game\User data\";
+    
+    public static List<GameItem> ConvertData (string path)
+    {
+        string jsonData = File.ReadAllText(basePath + path);
+        dynamic data = JsonConvert.DeserializeObject(jsonData);
+        var jsonItems = data.items;
+        
+        foreach (dynamic item in jsonItems)
+        {
+            int Id = unchecked((int)item.ItemTypeID.Value);
+            int Price = unchecked((int)item.Price.Value);
+           GameItems.Add(new GameItem(Id, item.Name.ToString(), Price));
+        }
+
+        return GameItems;
+    }
+
+}
