bd86645da818ab250c7ca6906cb4d87c96c4ba31
oleh.strokan <oleg14ua71@gmail.com>
2022-12-26 18:45:58 +0100

    Add Convert To Trader function

---
+++ Engine/Utils/ConvertToTrader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Engine.Factories;
+using Engine.Models;
+using Newtonsoft.Json;
+
+namespace Engine.Utils;
+
+public static class ConvertToTrader
+{
+    public static List<Trader> Traders = new List<Trader>();
+    public static string basePath = @"C:\Users\oleh.strokan\RiderProjects\rpg-game\User data\";
+    
+    public static List<Trader> ConvertData (string path)
+    {
+        string jsonData = File.ReadAllText(basePath + path);
+        dynamic data = JsonConvert.DeserializeObject(jsonData);
+        var jsonItems = data.traders;
+
+        
+        foreach (dynamic trader in jsonItems)
+        {
+            Trader newTrader = new Trader(trader.Name.ToString());
+             foreach (var items in trader.Items)
+             {
+                 int item = unchecked((int)items.ItemId);
+                 newTrader.AddItemToInventory(ItemFactory.CreateGameItem(item));
+             }
+            Traders.Add(newTrader);
+           
+        }
+
+        return Traders;
+    }
+
+}
