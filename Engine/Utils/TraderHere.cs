4245adae7402d585f54893860e8172d2b737064b
oleh.strokan <oleg14ua71@gmail.com>
2022-12-28 09:12:10 +0100

    Add traderHere helped class

---
+++ Engine/Utils/TraderHere.cs
@@ -0,0 +1,11 @@
+﻿namespace Engine.Utils;
+
+public  class TraderHere : LocationBase
+{
+    public string TraderName;
+
+    public TraderHere(string traderName, string locationName) : base(locationName)
+    {
+        TraderName = traderName;
+    }
+}
