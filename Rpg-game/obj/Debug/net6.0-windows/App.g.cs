c540101d776126a9f71abcd4dfb0af1713393579
oleh.strokan <oleg14ua71@gmail.com>
2022-12-30 19:51:16 +0100

    Update solution

--- Rpg-game/obj/Debug/net6.0-windows/App.g.cs
+++ Rpg-game/obj/Debug/net6.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace Rpg_game {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace Rpg_game {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public static void Main() {
             Rpg_game.App app = new Rpg_game.App();
             app.InitializeComponent();
