666774b6557d7938a83cce97141054dbb7ec7364
oleh.strokan <oleg14ua71@gmail.com>
2022-12-30 19:51:53 +0100

    Add unit test template

---
+++ TestEngine/ViewModels/UnitTest11.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestEngine.ViewModels
+{
+    [TestClass]
+    public class UnitTest11
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+        Assert
+        }
+    }
+}
